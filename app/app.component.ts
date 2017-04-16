a659cd09fc02bc375524b45ba376db0ebc135b82
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2017-04-16 17:45:39 +0100

    Observables using ReactiveX

---
+++ app/app.component.ts
@@ -0,0 +1,54 @@
+/// <reference path="../typings/tsd.d.ts" />
+
+import {Component} from 'angular2/core';
+import {Observable} from 'rxjs/Rx';
+import {ControlGroup, FormBuilder} from 'angular2/common';
+
+
+
+
+@Component({
+    selector: 'my-app',
+    template: `
+         <input id="search" type="text" class="form-control"> 
+      
+    `
+})
+//ReactiveX
+export class AppComponent {
+
+
+    constructor(){
+        var keyups = Observable.fromEvent($("#search"),"keyup")
+        .map(e=> e.target.value)
+        .filter(text => text.length >=3)
+        .debounceTime(400)
+        .distinctUntilChanged()
+        .flatMap(searchTerm=>{
+            var url = "https://api.spotify.com/v1/search?type=artist&q=" + searchTerm;
+            var promise =$.getJSON(url);
+            return Observable.fromPromise(promise);           
+        });        
+        
+      var subscription = keyups.subscribe(data=>console.log(data));
+      subscription.unsubscribe();
+  
+        // var debounced = _.debounce(function(text)){
+        //      var url = "https://api.spotify.com/v1/search?type=artist&q=" + text;
+        //     $.getJSON(url, function(artists){
+        //         console.log(artists);
+        //         });
+        // }, 400);
+
+        // $("#search").keyup(function(e){
+        //     var text = e.target.value;
+
+        //     if (text.length<3)
+        //     return;
+
+        //     debounced(text);           
+            
+        // });
+    }
+    
+}
